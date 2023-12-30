3475798cd6eac11eac3530815f52692d5f6c02fa
Jack <jglenn1038@gmail.com>
2023-12-30 15:41:05 -0600

    meta tags added

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 import express from "express";
-import fs from "fs";
-import path from "path";
 import url from "url";
 
 const app = express();
 const port = 8000
-// console.log(import.meta.url);
 const root = url.fileURLToPath(new URL('.', import.meta.url));
-// console.log(root);
-
 
 app.get("/", (request, response) => {
     const options = {
         root: root
     };
- 
     response.sendFile("./index.html", options)
 })
 
