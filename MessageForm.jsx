70671507bc78823db4d7f5299d695d5870837f17
Jack <jglenn1038@gmail.com>
2023-12-30 21:40:12 -0600

    client attempts to reconnect to server if websocket was closed

--- MessageForm.jsx
+++ MessageForm.jsx
@@ -1,62 +1,72 @@
 import { useState } from 'react';
 import React from "react";
-// const socket = new WebSocket("ws://localhost:8080");
+// const socket = new WebSocket(":25
 
-function waitForOpen(socket) {
-    return new Promise((resolve, reject) => {
-        const maxAttempts = 5;
-        const delayBetweenAttempts = 100;
-        let i = 0;
 
-        const attempt = () => {
-            console.log(`${i} attempts to open connection`);
-            setTimeout(() => {
-                if (socket.readyState === socket.OPEN) {
-                    resolve();
-                } else if (i < maxAttempts) {
-                    i++;
-                    attempt();
-                } else {
-                    reject(new Error("Exceded Maximimum number of attempts to connect to socket"))
-                }
-            }, delayBetweenAttempts);
-        }
-        attempt();
-    })
-}
 
-async function sendMessage(socket, message) {
-    console.log(socket);
-    if (socket.readyState === socket.OPEN) {
-        socket.send(message);
-    } else {
-        try {
-            await waitForOpen(socket);
-            socket.send(message);
-        } catch (err) {
-            console.error(err);
-        }
-    }
-}
+
+
 
 class MessageForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             message: "",
-            ws: new WebSocket("ws://localhost:8080")
+            socket: new WebSocket("ws://localhost:8080")
         }
-        this.wsURL = "ws://localhost:8080";
+        this.socketURL = "ws://localhost:8080";
         this.sendMessage = this.sendMessage.bind(this);
+        this.sendMessageHandler = this.sendMessageHandler.bind(this);
         this.handleChange = this.handleChange.bind(this);
-
+        this.waitForOpen = this.waitForOpen.bind(this);
     }
 
+    async sendMessage() {
+        console.log(this.state.socket);
+        if (this.state.socket.readyState === this.state.socket.OPEN) {
+            socket.send(this.state.message);
+        } else {
+            try {
+                await this.waitForOpen(this.state.socket);
+                this.state.socket.send(this.state.message);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
 
-    sendMessage(event) {
+    waitForOpen() {
+        return new Promise((resolve, reject) => {
+            const maxAttempts = 5;
+            const delayBetweenAttempts = 100;
+            let i = 0;
+    
+            // TODO attempt to reconnect if closed, 
+            const attempt = () => {
+                console.log(`${i} attempts to open connection`);
+                if (this.state.socket.readyState === this.state.socket.CLOSED) {
+                    console.log(`Re-Establishing connection to ${this.state.socket.url}`)
+                    this.state.socket = new WebSocket(this.state.socket.url);
+                }
+                setTimeout(() => {
+                    if (this.state.socket.readyState === this.state.socket.OPEN) {
+                        resolve();
+                    } else if (i < maxAttempts) {
+                        i++;
+                        attempt();
+                    } else {
+                        reject(new Error("Exceded Maximimum number of attempts to connect to socket"))
+                    }
+                }, delayBetweenAttempts);
+            }
+            attempt();
+        })
+    }
+
+    sendMessageHandler(event) {
         event.preventDefault();
         console.log(`tying to send: ${this.state.message}`);
-        sendMessage(this.state.ws, this.state.message);
+        this.sendMessage(this.state.socket, this.state.message);
         // const socket = new WebSocket("ws://localhost:8080", () => {
         //     socket.send(this.state.message);
         // });
@@ -69,7 +79,7 @@ class MessageForm extends React.Component {
 
     render() {
         return(
-            <form onSubmit={this.sendMessage}>
+            <form onSubmit={this.sendMessageHandler}>
                 <label>Send Message
                     <input type="text" value={this.state.value} onChange={this.handleChange} />
                 </label>
