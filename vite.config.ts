c1cb77dcfba469586f62d4efea2326ff5fc9b210
Jack <jglenn1038@gmail.com>
2024-04-09 14:15:46 -0400

    initial migration work

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8000,
-  },
-  build: {
-    minify: false,
-  },
+  // server: {
+  //   port: 8000,
+  // },
+  // build: {
+  //   minify: false,
+  // },
 })
