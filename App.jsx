f1ff57f970ad9c4571bcc22855f10ee86866fab7
Jack <jglenn1038@gmail.com>
2024-04-09 10:36:04 -0400

    auto scrolling to bottom of message list when a new message is added

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import MessageForm from "./MessageForm.jsx";
 import { WSProvider } from "./WebSocketProvider.jsx";
@@ -7,12 +7,33 @@ import { WSProvider } from "./WebSocketProvider.jsx";
 
 function App() {
     const [messageList, setMessageList] = useState([]);
+    const messageAreaRef = useRef(null);
+    const messageAreaBottomRef = useRef(null);
+    const [atBottomBool, setAtBottomBool] = useState(true);
 
     const dataTransfer = (message) => {
-        console.log("in data transfer");
-        // console.log(message);
-        setMessageList([...messageList, message])
+        console.log("in data transfer, appending: " + message);
+        setMessageList(messageList.concat(message));
+        if (messageAreaRef.current.scrollHeight - Math.round(messageAreaRef.current.scrollTop) === messageAreaRef.current.clientHeight) {
+            setAtBottomBool(true);
+        } else {
+            setAtBottomBool(false);
+        }
     }
+
+    const checkScrollUpdate = () => {
+        console.log(messageAreaRef.current.scrollHeight - Math.round(messageAreaRef.current.scrollTop));
+        console.log(messageAreaRef.current.offsetHeight);
+        console.log(atBottomBool);
+        if (atBottomBool) {
+            console.log("should scroll");
+            messageAreaBottomRef.current.scrollIntoView({ behavior: "smooth"});
+        }
+    };
+
+    useEffect(() => {
+        checkScrollUpdate()
+    }, [messageList]);
 
     const list = messageList.map((val) => (
         <p className="message">{val}</p>
@@ -21,8 +42,9 @@ function App() {
     return(
         <main>
             <WSProvider>
-            <div className="messageArea">
+            <div className="messageArea" ref={messageAreaRef}>
                 {list}
+                <div ref={messageAreaBottomRef} />
             </div>
             <MessageForm dataTransfer={dataTransfer}/>
             </WSProvider>
