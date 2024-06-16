4dd6efedb590d799066d6d8c0000a9914ae2daaf
Jack <jglenn1038@gmail.com>
2024-06-16 16:55:50 -0500

    added key to list item

--- src/MessageArea.tsx
+++ src/MessageArea.tsx
@@ -8,46 +8,66 @@ export default function MessageArea() {
     const messageAreaBottomRef = useRef<HTMLDivElement>(null);
     const [atBottomBool, setAtBottomBool] = useState<boolean>(true);
 
-    const dataTransfer = useCallback((message: string[]) => {
-        console.log("in data transfer, appending: " + message);
-        setMessageList(messageList.concat(message));
-        if (!messageAreaRef.current) throw Error("messageAreaRef is not assigned");
-        if (messageAreaRef.current.scrollHeight - Math.round(messageAreaRef.current.scrollTop) === messageAreaRef.current.clientHeight) {
-            setAtBottomBool(true);
-        } else {
-            setAtBottomBool(false);
-        }
-    }, [messageList]);
+    const dataTransfer = useCallback(
+        (message: string[]) => {
+            console.log("in data transfer, appending: " + message);
+            setMessageList(messageList.concat(message));
+            if (!messageAreaRef.current)
+                throw Error("messageAreaRef is not assigned");
+            if (
+                messageAreaRef.current.scrollHeight -
+                    Math.round(messageAreaRef.current.scrollTop) ===
+                messageAreaRef.current.clientHeight
+            ) {
+                setAtBottomBool(true);
+            } else {
+                setAtBottomBool(false);
+            }
+        },
+        [messageList]
+    );
 
     const checkScrollUpdate = () => {
-        if (!messageAreaRef.current) throw Error("messageAreaRef is not assigned");
-        console.log(messageAreaRef.current.scrollHeight - Math.round(messageAreaRef.current.scrollTop));
+        if (!messageAreaRef.current)
+            throw Error("messageAreaRef is not assigned");
+        console.log(
+            messageAreaRef.current.scrollHeight -
+                Math.round(messageAreaRef.current.scrollTop)
+        );
         console.log(messageAreaRef.current.offsetHeight);
         console.log(atBottomBool);
         if (atBottomBool) {
             console.log("should scroll");
-            if (!messageAreaBottomRef.current) throw Error("messageAreaBottomRef is not assigned");
-            messageAreaBottomRef.current.scrollIntoView({ block:"end", inline:"nearest", behavior: "smooth"});
+            if (!messageAreaBottomRef.current)
+                throw Error("messageAreaBottomRef is not assigned");
+            messageAreaBottomRef.current.scrollIntoView({
+                block: "end",
+                inline: "nearest",
+                behavior: "smooth",
+            });
         }
     };
 
     useEffect(() => {
-        checkScrollUpdate()
-    },);
+        checkScrollUpdate();
+    });
 
-    const list = useMemo(() => messageList.map((val) =>
-        <Message
-            message = {val}
-        />
-    ), [messageList]);
+    // TODO make sure key id isnt created on every render
+    const list = useMemo(
+        () =>
+            messageList.map((val) => (
+                <Message message={val} key={crypto.randomUUID()} />
+            )),
+        [messageList]
+    );
 
     return (
         <div>
             <div className="messageArea" ref={messageAreaRef}>
-            {list}
-            <div ref={messageAreaBottomRef} />
+                {list}
+                <div ref={messageAreaBottomRef} />
             </div>
-            <MessageForm dataTransfer={dataTransfer}/>
+            <MessageForm dataTransfer={dataTransfer} />
         </div>
-    )
+    );
 }
