42f3182491123c3abd24eb2b5d0216012d787032
Jack <jglenn1038@gmail.com>
2024-06-21 22:01:53 -0500

    basic profile picture rendering added

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 
 type MessageProp = {
     message: string;
@@ -5,8 +6,10 @@ type MessageProp = {
 
 export default function Message(props: MessageProp) {
     return(
-        <div className="message">
-            <p>{props.message}</p>
+        <div className={cn("pl-20 relative")}>
+            <img src="/res/default-pfp.jpg" className={cn("rounded-[50%] max-w-10 absolute left-5")}/>
+            <h1 className={cn("mb-2")}>UserName</h1>
+            <p className={cn("rounded-lg w-max max-w-full px-3 py-2 text-sm bg-primary text-primary-foregroun whitespace-pre-line text-wrap")}>{props.message}</p>
         </div>
     );
 }
