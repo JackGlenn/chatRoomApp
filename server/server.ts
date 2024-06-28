74e8c28e309dcdf20e5555f99a230f80bd8b592f
Jack <jglenn1038@gmail.com>
2024-06-28 14:48:04 -0400

    added try catch to asyncronous actions, undefined checking and refined pg insert queries

--- server/server.ts
+++ server/server.ts
@@ -20,18 +20,25 @@ const pool = new Pool({
 
 async function insertMessage(message: string) {
     const obj = JSON.parse(message);
-    return pool.query(
-        "INSERT INTO messages VALUES(DEFAULT, $1, $2, $3, $4) RETURNING *",
-        [obj.message_text, obj.post_time, 1, 1]
-    );
+    try {
+        return pool.query(
+            "INSERT INTO messages VALUES(DEFAULT, $1, $2, $3, $4) RETURNING message_id, message_text, post_time",
+            [obj.message_text, obj.post_time, 1, 1]
+        );
+    } catch (err) {
+        console.error(err);
+    }
 }
 
-
 async function loadTenMessages(socket: WebSocket) {
-    const res = await pool.query(
-        "SELECT message_id, message_text, post_time FROM messages ORDER BY post_time DESC LIMIT 10;"
-    );
-    socket.send(JSON.stringify(res.rows));
+    try {
+        const res = await pool.query(
+            "SELECT message_id, message_text, post_time FROM messages ORDER BY post_time DESC LIMIT 10;"
+        );
+        socket.send(JSON.stringify(res.rows));
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 const __filename = fileURLToPath(import.meta.url);
@@ -42,18 +49,24 @@ const port = process.env.PORT;
 
 const wsServer = new WebSocketServer({ port: parseInt(process.env.WSPORT!) });
 
-
 wsServer.on("connection", (socket) => {
     loadTenMessages(socket);
     console.log("number of connected clients: ", wsServer.clients.size);
     socket.on("message", async (message) => {
         const convertedMessage: string = message.toString();
-        const insertedMessage = await insertMessage(convertedMessage);
-        const toSend = JSON.stringify(insertedMessage.rows);
-        wsServer.clients.forEach((client) => {
-            console.log("sending: ", toSend);
-            client.send(toSend);
-        });
+        try {
+            const insertedMessage = await insertMessage(convertedMessage);
+            // Undefined check
+            if (insertedMessage) {
+                const toSend = JSON.stringify(insertedMessage.rows);
+                wsServer.clients.forEach((client) => {
+                    console.log("sending: ", toSend);
+                    client.send(toSend);
+                });
+            }
+        } catch (err) {
+            console.error(err);
+        }
     });
 });
 
