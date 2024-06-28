9e1587761e51f045d355ce8412f416ac981d624e
Jack <jglenn1038@gmail.com>
2024-06-28 13:20:21 -0400

    square aspect ratio added on profile picture

--- src/Message.tsx
+++ src/Message.tsx
@@ -8,7 +8,7 @@ type MessageProp = {
 export default function Message({message, username}: MessageProp) {
     return(
         <div className={cn("pl-20 relative")}>
-            <img src="/res/default-pfp.jpg" className={cn("rounded-[50%] max-w-10 absolute left-5")}/>
+            <img src="/res/default-pfp.jpg" className={cn("rounded-[50%] max-w-10 absolute left-5 aspect-square")}/>
             <h1 className={cn("mb-2")}>{username}</h1>
             <p className={cn("rounded-lg w-max max-w-full px-3 py-2 text-sm bg-primary text-primary-foregroun whitespace-pre-line text-wrap")}>{message}</p>
         </div>
